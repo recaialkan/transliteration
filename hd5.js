57d3fa05f501ea6c581d52f7558ed608a14b2f8b
recaialkan <recai_alkan@hotmail.com>
2016-02-23 22:21:10 +0200

    Added files via upload
    Harf alfabe dönüşüm, transliteration ancak 3. Döngüsü sapıtıyor

---
+++ hd5.js
@@ -0,0 +1,116 @@
+var DİZİ = [
+    ['YA', 'A', 'YE', 'YU', 'E', 'İ', 'NÇ', 'NC', 'NG', 'NY', 'NT', 'ND', 'N', 'R', 'LT', 'LD', 'L', 'K', 'Q', 'K', 'C', 'J', 'D', 'I', 'TS', 'T', 'PS', 'S', 'M', 'U', 'YO', 'Y', 'O', 'B', 'Ü', 'Ə', 'ŞÇ', 'Ş', 'Ğ', 'G', 'V', 'Ç', 'Z', 'Ö', 'H', 'H', 'P', 'X', 'F', 'Θ', 'W', 'Δ', 'T', 'Δ', 'S', 'Δ', 'Ê', 'Ô', "'", '', '', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+     ['ya', 'a', 'ye', 'yu', 'e', 'i', 'nç', 'nc', 'ng', 'ny', 'nt', 'nd', 'n', 'r', 'lt', 'ld', 'l', 'k', 'q', 'k', 'c', 'j', 'd', 'ı', 'ts', 't', 'ps', 's', 'm', 'u', 'yo', 'y', 'o', 'b', 'ü', 'Ə', 'şç', 'ş', 'ğ', 'g', 'v', 'ç', 'z', 'ö', 'h', 'h', 'p', 'x', 'f', 'θ', 'w', 'δ', 't', 'δ', 's', 'δ', 'ê', 'Ô', "'", '', '', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+     ['ΙΑ', 'Α', 'ΙΕ', 'ΙΟΥ', 'Ε', 'Ι', 'ΝΞ', 'ΝΓ', 'ΝΓ', 'ΝΙ', 'ΝΤ', 'ΝΔ', 'Ν', 'Ρ', 'ΛΤ', 'ΛΔ', 'Λ', 'Κ', 'Κ', 'Κ', 'Γ', 'ΔΖ', 'Δ', 'Ι', 'ΤΣ', 'Τ', 'Ψ', 'Θ', 'Μ', 'ΟΥ', 'ΙΟ', 'Ι', 'Ο', 'Β', 'Υ', 'Ε', 'ΣΞ', 'Σ', 'ΓΧ', 'Γ', 'Β', 'Ξ', 'Ζ', 'Ο', 'Χ', 'Χ', 'Π', 'Χ', 'Φ', 'Θ', 'Β', 'Ζ', 'Τ', 'Τ', 'Σ', 'Δ', 'Η', 'Ω', "'", '', '', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+     ['ια', 'α', 'ιε', 'ιου', 'ε', 'ι', 'νξ', 'νγ', 'νγ', 'νι', 'ντ', 'νδ', 'ν', 'ρ', 'λτ', 'λδ', 'λ', 'κ', 'κ', 'κ', 'γ', 'δζ', 'δ', 'ι', 'τσ', 'τ', 'ψ', 'θ', 'μ', 'ου', 'ιο', 'ι', 'ο', 'β', 'υ', 'ε', 'σξ', 'σ', 'γχ', 'γ', 'β', 'ξ', 'ζ', 'ο', 'χ', 'χ', 'π', 'χ', 'φ', 'θ', 'β', 'ζ', 'τ', 'τ', 'σ', 'δ', 'η', 'ω', "'", '', '', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+     ['Я', 'А', 'Е', 'Ю', 'Э', 'И', 'НЧ', 'НДЖ', 'НГ', 'НЙ', 'НТ', 'НД', 'Н', 'Р', 'ЛТ', 'ЛД', 'Л', 'К', 'К', 'К', 'ДЖ', 'Ж', 'Д', 'Ы', 'Ц', 'Т', 'ПС', 'С', 'М', 'У', 'Ё', 'Й', 'О', 'Б', 'Ю', 'Э', 'Щ', 'Ш', 'ГХ', 'Г', 'В', 'Ч', 'З', 'Ё', 'Х', 'Х', 'П', 'Х', 'Ф', 'Т', 'В', 'З', 'Т', 'Т', 'С', 'Д', 'Е', 'О', "'", 'Ъ', 'Ь', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+     ['я', 'а', 'е', 'ю', 'э', 'и', 'нч', 'ндж', 'нг', 'нй', 'нт', 'нд', 'н', 'р', 'лт', 'лд', 'л', 'к', 'к', 'к', 'дж', 'ж', 'д', 'ы', 'ц', 'т', 'пс', 'с', 'м', 'у', 'ё', 'й', 'о', 'б', 'ю', 'э', 'щ', 'ш', 'гх', 'г', 'в', 'ч', 'з', 'ё', 'х', 'х', 'п', 'х', 'ф', 'т', 'в', 'з', 'т', 'т', 'с', 'д', 'е', 'о', "'", 'ъ', 'ь', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+     ['ىا', 'ا', 'ىھ', 'ىو', 'ٲ', 'ى', 'نچ', 'نج', 'نغ', 'نى', 'نت', 'ند', 'ن', 'ر', 'لت', 'لد', 'ل', 'ق', 'ك', 'ق', 'ج', 'ژ', 'د', 'ى', 'ث', 'ت', 'پس', 'س', 'م', 'و', 'ىو', 'ى', 'و', 'ب', 'و', 'ا', 'شچ', 'ش', 'غ', 'غ', 'و', 'چ', 'ز', 'و', 'ح', 'ھ', 'پ', 'خ', 'ف', 'ث', 'و', 'ذ', 'ط', 'ظ', 'ص', 'ض', 'ھ', 'و', 'ع', '', '', '٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩']
+];
+
+// var abece = /^[a-zA-ZÂâÎîİıÇçŞşÜüÖöĞğ]/;
+var sesliler = 'AaÂâEeIıİiÎîOoÖöUuÜü';
+var t = 0;
+var p = 0;
+var çmd = ['','','','','','']
+
+function ters(m) {
+    return m.split("").reverse().join("");
+    // String.prototype.reverse=function(){return this.split("").reverse().join("");}
+}
+
+function sesli(h) {
+    return sesliler.includes(h);
+}
+
+function hecele(mg) {
+    var mc = "";
+    p = 0;
+    t = mg.length-1;
+    mc = hece(mg,0,t);
+    var m3 = mc.replace(/- /g," ");
+    var m4 = m3.replace(/ -/g," ");
+    m3 = m4.substring(0,m4.length-1);
+    mc = m3;
+    return mc;
+
+    function hece(mg, i, j) {
+        var k=0;
+        j = t;
+        while (t>=0 && !sesli(mg[t]))	//sesliyi bul
+            t--;
+        if (t>=0 && sesli(mg[t])) {
+            t--;
+            if (t>=0 && !sesli(mg[t]))
+                t--;
+            if (t==0 && !sesli(mg[t]))
+                t--;
+        }
+        i = t+1;
+        if (t>=0)
+            hece(mg,0,j);
+        else
+            t=0;
+        for(k=i; k<=j; k++)
+            mc += mg[k];
+        mc += '-';
+        return mc;
+    }
+}
+
+function metDönüş(gm) {
+    var tr = 0;     // girdi tarayıcı
+    var tdh =[];    // [td, th] tara dil harf
+    var huz = 0;     // harf uzunluğu
+    var hd = 0;     // hedef dil
+    var hdf = 0;    // hedef dil fark
+    var hdt = 0;    // hedef dil küçük/büyük
+    
+    for (tr=0; tr<gm.length; tr += huz) { // girdi tara
+        tdh= harfBul(tr);
+        if (tdh) {
+            for (hd=0; hd<4; hd++) { // hedef dil
+                hdf = hd<3? 2*(hd-Math.floor(tdh[0]/2)) :6-tdh[0];
+                if (tdh[0]==6)
+                    hdf++;
+                hdt = tdh[0]+hdf;
+                çmd[hd] += DİZİ[hdt][tdh[1]];
+            }
+            alert ('tr '+tr+'  td '+tdh[0]+' '+'b+'+'  huz '+huz);
+        }
+        else {
+            for (hd=0; hd<4; hd++)
+                çmd[hd] += gm.substring(tr,1); 
+        alert ('tr '+tr+' '+'b-'+'  huz '+huz);
+        }
+    }
+    return;
+    
+    function harfBul (trr) {
+        var tdd;
+        var thh;
+        for (tdd =0; tdd<7; tdd++) { // dizi-dil
+            for (thh=0; thh<71; thh++) { // dizi harf
+                if (DİZİ[tdd][thh]) { // boş veya null değilse
+                    huz = DİZİ[tdd][thh].length; // harf uzunluğu
+                    if (DİZİ[tdd][thh] == gm.substring(trr,huz)) {
+                        return [tdd, thh]; }
+            } } }
+        return null;
+    }
+}
+
+$(document).ready (function() {
+	console.log("doc ready");
+	$(say).click(function() {
+        console.log("doc ready", $('#metin1').val());
+		çmd[4] = $('#metin1').val();
+        çmd[5] = hecele(çmd[4]);
+        metDönüş(çmd[4]);
+        document.getElementById("metin2").value = çmd[5];
+        document.getElementById("metin3").value = çmd[0];
+        document.getElementById("metin4").value = çmd[1];
+        document.getElementById("metin5").value = çmd[2];
+        document.getElementById("metin6").value = çmd[3];
+    });
+});
